e3c4ef037d8f445c69d82665e774c60a38fecc37
AMANTEMBHARE <amantembhare07@gmail.com>
2026-08-14 17:57:16 +0530

    authorization done

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authenticate = require("../middleware/auth.middleware");
+const { authorizeRoles } = require("../middleware/authorization.middleware");
 
 const router = express.Router();
 
@@ -7,6 +8,14 @@ router.get("/users/profile", authenticate, (req, res) => {
     res.json({
         success: true,
         message: "You are authenticated",
+        user: req.user
+    });
+});
+
+router.get("/users/owner-area", authenticate, authorizeRoles("owner"), (req, res) => {
+    res.json({
+        success: true,
+        message: "welcome to the PG owner area",
         user: req.user
     });
 });
